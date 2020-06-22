ad9b0714b3399c97d3936e7050e2fd88b0988297
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-06-22 18:35:58 -0500

    ghjghj

--- inc/post_layout/single-default-readopt.php
+++ inc/post_layout/single-default-readopt.php
@@ -3,31 +3,13 @@
 <?php wp_enqueue_script('rehubwaypoints' );wp_enqueue_script('rhreadingprogress' );?>
 <!-- CONTENT -->
 
-<div ng-app="domired" ng-controller="menu">
-    <div class="stv-tabs row-wrap center-center ">
-        <div class="s-flex comercio-tabs" ng-class="{'active-item': tab === 1  }" ng-click="tab = 1">
+<div ng-app="domired" ng-controller="menu" >
+    <div class="stv-tabs row-wrap center-center" ng-cloak>
+        <div class="s-15 comercio-tabs" ng-repeat="categoria in categorias" ng-class="{'active-item': cat_select === categoria.term_id  }" ng-click="changeCat(categoria.term_id)">
             <span class="mkdf-tour-nav-section-icon dripicons-document"></span>
-            <span class="mkdf-tour-nav-section-title">
-                <?php echo _x('Menu', 'nav menu single page comercio', 'roam-child');  ?> </span>
-        </div>
-
-        <div class="s-flex comercio-tabs" ng-class="{'active-item': tab === 2 }" ng-click="tab = 2">
-            <span class="mkdf-tour-nav-section-icon dripicons-map"></span>
-            <span class="mkdf-tour-nav-section-title">
-            <?php echo _x('Historia', 'nav menu single page comercio', 'roam-child');  ?> </span>
+            <span class="mkdf-tour-nav-section-title">{{categoria.name}}</span>
         </div>
         
-        <div class="s-flex comercio-tabs" ng-class="{'active-item': tab === 3 }" ng-click="tab = 3">
-            <span class="mkdf-tour-nav-section-icon dripicons-location"></span>
-            <span class="mkdf-tour-nav-section-title">
-            <?php echo _x('Ubicación', 'nav menu single page comercio', 'roam-child');  ?> </span>
-        </div>
-
-        <div class="s-flex comercio-tabs" ng-class="{'active-item': tab === 4 }" ng-click="tab = 4">
-            <span class="mkdf-tour-nav-section-icon dripicons-camera"></span>
-            <span class="mkdf-tour-nav-section-title">
-            <?php echo _x('Comentarios', 'nav menu single page comercio', 'roam-child');  ?> </span>
-        </div>
     </div>
     <div class="rh-container"> 
         <div class="rh-content-wrap clearfix"  >
@@ -50,12 +32,27 @@
                                         'terms'    => array ($marca))
                                     )
                                 );
+
+                            $categorias = [];
+
                             $loop = new WP_Query( $args );
                             if ( $loop->have_posts() ) {
                                 while ( $loop->have_posts() ) : $loop->the_post(); 
-                                ?>
+                                
+                                $terms = get_the_terms( $loop->post->ID, 'product_cat' );
+                                $cats_item_ids = [];
+                                for($i = 0; $i < count($terms); $i++){
+                                    $categorias[$terms[$i]->name] = $terms[$i];
+                                    $cats_item_ids[$i] = $terms[$i]->term_id;
+                                }
+                                if(empty($cat_items_ids)){
+                                    $cat_items_ids = [0];
+                                }
+                                
 
-                                    <div class="row stv-producto-item mobile-not" data-profile="<?php echo $loop->post->ID; ?>">
+                                ?>
+                                
+                                    <div ng-hide="-1 === <?php echo json_encode($cats_item_ids, true); ?>.indexOf(cat_select)" class="row stv-producto-item mobile-not" data-profile="<?php echo $loop->post->ID; ?>">
                                       
                                         <div class="stv-producto-img-container">
                                             
@@ -109,7 +106,7 @@
 
                                     </div>
 
-                                    <div class="stv-producto-item mobile" data-profile="<?php echo $loop->post->ID; ?>">
+                                    <div ng-hide="-1 === <?php echo json_encode($cats_item_ids, true); ?>.indexOf(cat_select)" class="stv-producto-item mobile" data-profile="<?php echo $loop->post->ID; ?>">
                                        
                                         <div class="row-wrap space-between-center">
                                             <h4 class="titulo"><?php the_title(); ?></h4>
@@ -164,13 +161,16 @@
                                               
 
                                     </div>
+                                
 
 
                                     <?php
                                 endwhile;
                             } else {
-                                echo __( 'No products found' );
+                                echo __( 'No se encontraron productos de este negocio' );
                             }
+
+                            echo '<script> var categorias_productos = '.json_encode($categorias, true).'</script>';
                             wp_reset_postdata();
                         ?>
                     
@@ -262,16 +262,10 @@
                             </div>
                             </div>
                         </div>
+                        
+                        <?php comments_template(); ?> 
                     </article>
                    
-
-                    <article ng-show="tab === 3" >
-                        MAPA
-                    </article>
-
-                    <article ng-show="tab === 4" >
-                       <?php comments_template(); ?> 
-                    </article>
                     <div class="clearfix"></div>                    
                 <?php endwhile; endif; ?>
                 <?php if(rehub_option('rehub_disable_relative') =='1' || vp_metabox('rehub_post_side.disable_parts') == '1')  : ?>
