375dce04ac7d242dbbc90c8230436a87f3b52652
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-07-15 21:58:53 -0500

    dsf

--- functions.php
+++ functions.php
@@ -79,7 +79,7 @@ function get_hability_horarios ( $id ){
 
 add_action( 'woocommerce_order_item_meta_start', 'ts_order_item_meta_start', 10, 4 );
 function ts_order_item_meta_start( $item_id, $item, $order, $plain_text ){
-    $product    = $item->get_product_id();
+    $product    = $item->get_product();
 	var_dump( '<p> NEGOCIO:',$product, get_the_terms($product,'pa_brand') ,'</p>');
 }
 
