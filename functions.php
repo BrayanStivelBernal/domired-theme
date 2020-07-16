a12988bc12e7bd73b53ab5b24844fe6f72d19da3
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-07-15 22:02:50 -0500

    marcas en correos

--- functions.php
+++ functions.php
@@ -80,7 +80,7 @@ function get_hability_horarios ( $id ){
 add_action( 'woocommerce_order_item_meta_start', 'ts_order_item_meta_start', 10, 4 );
 function ts_order_item_meta_start( $item_id, $item, $order, $plain_text ){
     $product    = $item->get_product_id();
-	var_dump( '<p style="color:red; font-weight:600; text-transform:uppercase;"> NEGOCIO: ',$product, get_the_terms($product,'store')[0]->name ,'</p>');
+	echo '<p style="color:red; font-weight:600; text-transform:uppercase;"> NEGOCIO: ',$product, get_the_terms($product,'store')[0]->name ,'</p>';
 }
 
 ?>
